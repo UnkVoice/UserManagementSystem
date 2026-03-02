d77b49161928bea7453ba09419592fa5e099e7c5
Chara <Chara@DESKTOP-JRCG2NI>
2026-03-02 19:49:09 +0300

    "Добавлен базовый класс UserManager"

---
+++ UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem
+{
+    public class UserManager
+    {
+        private List _users = new List();
+
+        public void AddUser(string username)
+        {
+            _users.Add(username);
+        }
+    }
+}
